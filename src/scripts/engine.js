b54c37b9da269afcd96b28cd3f455b84b082c612
Júlio Queiroz <jcqaraujo@gmail.com>
2024-12-09 15:47:16 -0400

    doc: inicio da engine do jogo

---
+++ src/scripts/engine.js
@@ -0,0 +1,28 @@
+// state é um objeto de objetos, onde guardamos memoria 
+
+const state = {
+    score:{
+        playerScore: 0,
+        computerScore: 0,
+        scoreBox: document.getElementById('score_points')
+    },
+    cardSprite:{
+        avatar: document.getElementById('card-image'),
+        name: document.getElementById('card-name'),
+        type: document.getElementById('card-type'),
+    },
+    fieldCards:{
+        player: document.getElementById('player-field-card'),
+        computer: document.getElementById('computer-field-card')
+    },
+    button: document.getElementById('next-duel')
+};
+
+
+
+
+function init() {
+    
+}
+
+init();
