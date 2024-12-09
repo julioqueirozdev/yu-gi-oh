412b25b0aae5ac84d4ae0ac956b2008dccf0367d
Júlio Queiroz <jcqaraujo@gmail.com>
2024-12-09 16:59:35 -0400

    doc: implementação das funções

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -50,14 +50,42 @@ const cardData = [
         WinOf: [0],
         LoseOf: [1],
     }
-]
+];
 
-function drawCards(cardNumbers, fieldSide) {
+async function getRandomCardId() {
+    const randomIndex = Math.floor(Math.random() * cardData.length)
+    return cardData[randomIndex].id;
+}
+
+async function createCardImage(IdCard, fieldSide) {
+    const cardImage = document.createElement('img');
+    cardImage.setAttribute('height', '100px');
+    cardImage.setAttribute('src', '.src/assets/icons/card-back.png');
+    cardImage.setAttribute('data-id', IdCard);
+    cardImage.classList.add('card');
+
+    if(fieldSide === playerSides.player1){
+        cardImage.addEventListener('click', ()=> {
+            setCardsField(cardImage.getAttribute('data-id'))
+        })
+    }
+
+    cardImage.addEventListener('mouseover', () => {
+        drawSelectCard(IdCard);
+    });
+
+    return cardImage;
+
+}
+
+async function drawCards(cardNumbers, fieldSide) {
     for (let i = 0; i < cardNumbers; i++) {
         const randomIdCard = await getRandomCardId();
         const cardImage = await createCardImage(randomIdCard, fieldSide);
 
-        document.getElementById(fieldSide).appendChild(cardImage)
+        console.log(fieldSide)
+
+        document.getElementById(fieldSide).appendChild(cardImage);
 
     }
 }
